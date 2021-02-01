505a49a7f757de6633cd810ccdcd526fc9a46824
Hp <charukeshic-wm19@student.tarc.edu.my>
2021-02-01 15:48:01 +0800

    2 dices

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -20,14 +20,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll the dice
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
+
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+
+
 
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
         // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
+        val drawableResource = when (diceRoll1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource2 = when (diceRoll2) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -38,9 +53,11 @@ class MainActivity : AppCompatActivity() {
 
         // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
 
         // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
+        diceImage.contentDescription = diceRoll1.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 
     class Dice(private val numSides: Int) {
